77c2b44c6243c7c083ff2711d20aaa0afa46f634
JoSungHyeon <jsh950618@gmail.com>
2024-03-11 15:57:15 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(()=>{
+    console.log(`count: ${count} / iuput: ${input}`);
+  }, [count, input])
+  // 의존성 배열
+  // depth
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -13,6 +20,12 @@ function App() {
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e)=>{
+          setInput(e.target.value)
+        }}
+         />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
